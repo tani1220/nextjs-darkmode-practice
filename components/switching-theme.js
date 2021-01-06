8ae4882903dcc9bc83c0a0a1646346d8bce7b911
tani <berukoko1220-blog@yahoo.co.jp>
2021-01-06 13:11:44 +0900

    プラグインでダークモード実装

--- components/switching-theme.js
+++ components/switching-theme.js
@@ -2,50 +2,50 @@
 
   
  export default function SwitchingTheme() {
-    const setTheme = (newTheme) => {
-      const theme = !(newTheme === 'dark' || newTheme === 'light') ? 'light' : newTheme
-      document.documentElement.setAttribute('data-theme', theme) //htmlタグにdata-theme属性を付与して、見た目の切り替えを実現,任意の名前でOK
-      window.localStorage.setItem('theme', theme) //ローカルストレージにカラーモードの識別子: dark/lightを登録する.
-    }
-    const getTheme = () => {
-      const currentTheme = window.localStorage.getItem('theme')
-      if (currentTheme) {
-        return currentTheme
-      }
-      // ローカルストレージにデータ未登録の場合は、クライアントのシステムの外観モードで決定
-      // (メディアクエリの結果から判定)
-      const query = window.matchMedia('(prefers-color-scheme: dark)')
-      return (query.matches ? 'dark' : 'light')
-    }
-    const changeTheme = (e) => {
-      const currentTheme = getTheme()
-      const newTheme = (currentTheme === 'dark') ? 'light' : 'dark'
-      setTheme(newTheme)
-    }
-    setTheme(getTheme())
-  
-    return (
-      <>
-        <button type="button" onClick={changeTheme}>Switching Theme</button>
-        <style jsx>{`
-          button {
-            border: 0;
-            cursor: pointer;
-            outline: none;
-            padding: 10px 15px;
-            appearance: none;
-            background-color: var(--color-link);
-            border-radius: 5px;
-            color: var(--color-button-text);
-          }
-        `}</style>
-      </>
-    )
+  const setTheme = (newTheme) => {
+    const theme = !(newTheme === 'dark' || newTheme === 'light') ? 'light' : newTheme
+    document.documentElement.setAttribute('data-theme', theme) //htmlタグにdata-theme属性を付与して、見た目の切り替えを実現,任意の名前でOK
+    window.localStorage.setItem('theme', theme) //ローカルストレージにカラーモードの識別子: dark/lightを登録する.
   }
+  const getTheme = () => {
+    const currentTheme = window.localStorage.getItem('theme')
+    if (currentTheme) {
+      return currentTheme
+    }
+    // ローカルストレージにデータ未登録の場合は、クライアントのシステムの外観モードで決定
+    // (メディアクエリの結果から判定)
+    const query = window.matchMedia('(prefers-color-scheme: dark)')
+    return (query.matches ? 'dark' : 'light')
+  }
+  const changeTheme = (e) => {
+    const currentTheme = getTheme()
+    const newTheme = (currentTheme === 'dark') ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
+  setTheme(getTheme())
 
-  /** memo
-  * ? 条件 (三項) 演算子　https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
-  * localStorage  https://techacademy.jp/magazine/28182
-  * setAttribute https://developer.mozilla.org/ja/docs/Web/API/Element/setAttribute
-  * 参照サイト　https://koredana.info/blog/nextjs-switching-theme/
-  */
+  return (
+    <>
+      <button type="button" onClick={changeTheme}>Switching Theme</button>
+      <style jsx>{`
+        button {
+          border: 0;
+          cursor: pointer;
+          outline: none;
+          padding: 10px 15px;
+          appearance: none;
+          background-color: var(--color-link);
+          border-radius: 5px;
+          color: var(--color-button-text);
+        }
+      `}</style>
+    </>
+  )
+}
+
+/** memo
+* ? 条件 (三項) 演算子　https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+* localStorage  https://techacademy.jp/magazine/28182
+* setAttribute https://developer.mozilla.org/ja/docs/Web/API/Element/setAttribute
+* 参照サイト　https://koredana.info/blog/nextjs-switching-theme/
+*/
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider forcedTheme={Component.theme || undefined}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
-
-export default MyApp
