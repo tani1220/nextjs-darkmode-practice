e0efabed0a5b4397924deca3c1f17a3a7be484bb
tani <berukoko1220-blog@yahoo.co.jp>
2021-01-07 20:19:43 +0900

    commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return { assetPrefix: 'nextjs-darkmode-practice' };
+  }
+  return {};
+};
